73826b718d26501162f120d65171334bd8473f35
jihyeon-Do <ehwlgus22@gmail.com>
2025-03-11 19:08:01 +0900

    create project

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+const colors = {
+  boxBackground: "#F8F8F8",
+};
+
+const shadow = {
+  shadowColor: "3px 3px 8px rgba(0, 0, 0, 0.2);",
+};
+
+const theme = {
+  colors,
+  shadow,
+};
+
+export default theme;
