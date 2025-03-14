819cec3064cf24b3c82d52ab8068c9d16972637d
jihyeon-Do <ehwlgus22@gmail.com>
2025-03-15 02:46:45 +0900

    style : reset style

--- src/theme.ts
+++ src/theme.ts
@@ -1,14 +1,24 @@
 const colors = {
-  boxBackground: "#F8F8F8",
+  boxBackground: '#F8F8F8',
 };
 
 const shadow = {
-  shadowColor: "3px 3px 8px rgba(0, 0, 0, 0.2);",
+  shadowColor: '3px 3px 8px rgba(0, 0, 0, 0.2);',
+};
+
+/*
+최대 width : 640px 
+선정 이유 : tailwind css에서 sm의 최대 넓이가 640px로 설정되어있음
+*/
+
+const responsive = {
+  sm: '640px',
 };
 
 const theme = {
   colors,
   shadow,
+  responsive,
 };
 
 export default theme;
