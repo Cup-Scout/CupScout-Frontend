51d38415f3f36a2d47ea379ca67a9c3c0141115e
jihyeon-Do <ehwlgus22@gmail.com>
2025-03-19 23:18:52 +0900

    feat : Convert map address to coordinates

--- src/components/Maps.tsx
+++ src/components/Maps.tsx
@@ -1,18 +1,37 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
+
+interface cafeInfo {
+  id: number;
+  name: string;
+  address_doro: string;
+  lat: number;
+  lng: number;
+  description: string;
+  categories: string;
+  visible: number;
+  deleted: number;
+  created: string;
+  updated: string;
+}
 
 const Maps = () => {
   const mapRef = useRef<HTMLDivElement | null>(null);
-  const [map, setMapInstance] = useState<any>(null); // map 상태 추가
 
   //! 임시 position
   const cafePosition = [
-    { lat: 37.5213006, lng: 126.9288968 },
-    { lat: 37.4836332, lng: 126.9394218 },
-    { lat: 37.4837762, lng: 126.928656 },
-    { lat: 37.5677235, lng: 126.9704007 },
-    { lat: 37.5699321, lng: 126.9841797 },
-    { lat: 37.6488518, lng: 127.0137786 },
-    { lat: 37.5806659, lng: 127.0228185 },
+    {
+      id: 1,
+      name: '카페꼼마 여의도점',
+      address_doro: '서울 영등포구 국제금융로8길 16 1층 카페꼼마',
+      lat: 37.5213006,
+      lng: 126.9288968,
+      description: '문학동네에서 운영하는 대형 북카페',
+      categories: '1,2',
+      visible: 1,
+      deleted: 0,
+      created: '2025-02-14T06:24:38.000Z',
+      updated: '2025-03-10T21:21:32.000Z',
+    },
   ];
 
   const setMap = (naver: any, location: any) => {
@@ -33,28 +52,26 @@ const Maps = () => {
     }
   };
 
-  const initialMaps = () => {
-    const { naver } = window;
-    if (!naver) throw new Error('네이버 지도 API를 찾을 수 없습니다.');
-    if (mapRef.current) {
-      const location = new naver.maps.LatLng(37.5666103, 126.9783882);
-      const newMap = setMap(naver, location);
-      setMapInstance(newMap); // map 상태 업데이트
-    }
-  };
-
-  const getSuccess = (position: { lat: number; lng: number }[], map: any) => {
+  const getSuccess = async (cafe: cafeInfo[]) => {
     try {
       const { naver } = window;
       if (!naver) throw new Error('네이버 지도 API를 찾을 수 없습니다.');
+      let map;
+      if (mapRef.current) {
+        const location = new naver.maps.LatLng(37.5666103, 126.9783882);
+        map = setMap(naver, location);
+      }
       if (map) {
-        if (!Array.isArray(position) || position.length === 0) {
+        //: 주소를 좌표로 변환하여 요소 추가해서 배열로 만들기
+        const newCafe = await convertAddress(cafe);
+        console.log(newCafe);
+        if (!Array.isArray(newCafe) || newCafe.length === 0) {
           throw new Error('위치 데이터가 유효하지 않습니다.');
         }
-        for (let i = 0; i < position.length; i++) {
+        for (let i = 0; i < newCafe.length; i++) {
           try {
-            const latitude = position[i].lat;
-            const longitude = position[i].lng;
+            const latitude = newCafe[i].lat;
+            const longitude = newCafe[i].lng;
             const location = new naver.maps.LatLng(latitude, longitude);
             setMarker(map, location);
           } catch (error) {
@@ -79,14 +96,37 @@ const Maps = () => {
   };
 
   useEffect(() => {
-    initialMaps();
+    getSuccess(cafePosition);
   }, []);
 
-  useEffect(() => {
-    if (map) {
-      getSuccess(cafePosition, map);
+  const convertAddress = async (cafe: cafeInfo[]) => {
+    const geocodePromise = (
+      address: string,
+    ): Promise<{ lat: number; lng: number }> => {
+      return new Promise((resolve, reject) => {
+        naver.maps.Service.geocode({ query: address }, (status, response) => {
+          if (status === naver.maps.Service.Status.ERROR) {
+            return reject(new Error('Something wrong!'));
+          }
+          const lat = Number(response.v2.addresses[0].y);
+          const lng = Number(response.v2.addresses[0].x);
+          resolve({ lat, lng });
+        });
+      });
+    };
+    try {
+      const coordinateAddCafe = await Promise.all(
+        cafe.map(async (value) => {
+          //: map안에서 비동기
+          const { lat, lng } = await geocodePromise(value.address_doro);
+          return { ...value, lat, lng };
+        }),
+      );
+      return coordinateAddCafe;
+    } catch (error) {
+      console.log(error);
     }
-  }, [map]); // map이 생성된 후 getSuccess 실행
+  };
 
   return (
     <div id="map" ref={mapRef} style={{ width: '100%', height: '400px' }}>
