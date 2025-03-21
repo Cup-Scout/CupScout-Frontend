05b1216b289881ef277703c0d88d1388139f719f
jihyeon-Do <ehwlgus22@gmail.com>
2025-03-22 02:58:47 +0900

    chore : Temporary Test Map Component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import theme from './theme';
 import GlobalStyles from './globalStyle';
 import './App.css';
 import MainWrapper from './pages/MainWrapper';
-import MapsWrapper from './pages/MapsWrapper';
+// import MapsWrapper from './pages/MapsWrapper';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CafeWrapper from './pages/CafeWrapper';
 
@@ -15,7 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainWrapper />} />
           <Route path="/cafe" element={<CafeWrapper />} />
-          <Route path="/map" element={<MapsWrapper />} />
+          {/* <Route path="/map" element={<MapsWrapper />} /> */}
         </Routes>
       </ThemeProvider>
     </BrowserRouter>
---
+++ src/pages/MapsWrapper.tsx
@@ -0,0 +1,11 @@
+import Maps from '../components/Maps';
+
+const MapsWrapper = () => {
+  return (
+    <section className="wrapper">
+      <Maps />
+    </section>
+  );
+};
+
+export default MapsWrapper;
