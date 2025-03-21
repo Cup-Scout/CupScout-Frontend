026ce469a574f114f75cf280ef1a4ced01f57692
jihyeon-Do <ehwlgus22@gmail.com>
2025-03-22 02:59:04 +0900

    style : create main page

--- src/pages/MainWrapper.tsx
+++ src/pages/MainWrapper.tsx
@@ -1,9 +1,31 @@
-import Maps from '../components/Maps';
-
 const MainWrapper = () => {
   return (
     <main className="wrapper">
-      <Maps />
+      <nav>
+        <ul>
+          <li>
+            <span>📝</span>공부하기 좋은 카페
+          </li>
+          <li>
+            <span>📚</span>북카페
+          </li>
+          <li>
+            <span>🍧</span>빙수 맛집
+          </li>
+          <li>
+            <span>🎡</span>추억의 장소
+          </li>
+        </ul>
+      </nav>
+      <article>
+        <div>카페 이미지</div>
+        <div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+          facere saepe ullam adipisci fugit deleniti, magni, veniam,
+          perspiciatis laudantium nobis beatae? Temporibus odio rem repellendus
+          nobis, ipsum omnis tempora maiores.
+        </div>
+      </article>
     </main>
   );
 };
